e718c2e4cc2e5ff29520b984d9277ec004a06a55
Roman Hudec <roman.hudec@ksp.sk>
2016-05-17 20:00:34 +0200

    such about, much wow

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -7,6 +7,9 @@ export default class About extends Component {
       <div className="container">
         <h1>About</h1>
         <Helmet title="About"/>
+        <p>
+          This is a multiplayer arena game. Check it out! Just log in and click play!
+        </p>
       </div>
     );
   }
